eea84c03fc552c9c4898434c6cddde9952ed61e6
Aiden <krypn22@gmail.com>
2024-04-30 10:19:55 +0700

    try to remove cache from random api

--- services/banner.service.ts
+++ services/banner.service.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-export const bannerService = () => axios.get('api/banner').then(res => res.data.url)
+export const bannerService = () => axios.get(`api/banner?random=${Math.random() * 100000}`).then(res => res.data.url)
 
 export const bannerQuoteService = () => axios.get('api/quote/random').then(res => res.data)
